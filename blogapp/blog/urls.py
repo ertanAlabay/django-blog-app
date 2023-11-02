aac0dfbb8071d7e10459a8eb2ca9e3800f25ef41
Ertan Osman ALABAY <102266702+ertanAlabay@users.noreply.github.com>
2023-11-02 21:45:55 +0300

    Add files via upload

---
+++ blogapp/blog/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+#http://127.0.0.1:8000/         => index
+#http://127.0.0.1:8000/index    => index
+#http://127.0.0.1:8000/blogs    => blogs
+#http://127.0.0.1:8000/blogs/3  => blog-detail
+
+urlpatterns = [
+    path("", views.index,name="home"),
+    path("index", views.index),
+    path("blogs", views.blogs, name="blogs"),
+    path("blogs/<slug:slug>", views.blog_detail, name="blog_details"),
+    path("category/<slug:slug>", views.blogs_by_category, name="blogs_by_category")
+]
